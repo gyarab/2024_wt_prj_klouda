85ef3affa7bbfa3a0c9c5e9f9006f4e4a44e66b3
GeneralFlex <vincent.klouda.s@gyarab.cz>
2025-02-24 19:23:14 +0100

    exchange calculator

--- exchange.py
+++ exchange.py
@@ -1,25 +1,72 @@
 import httpx
+from colorama import Fore, init
 
 
 url = 'https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=13.02.2025'
 r = httpx.get(url)
+init()
 
-
-print(r.text)
-print(r.status_code)
+#print(r.text)
+#print(r.status_code)
 
 lines = r.text.split("\n")
 
+# metoda pro získání kurzu
+def get_kurz(currency):
+    row=0
+    if currency == "CZK":
+        return 1
+    curr = f"|{currency}|"
+    for line in lines:
+        if curr in line:
+           row = line
+    if row==0:
+        return -1
+
+    row_arr = row.split("|")
+
+    kurz = row_arr[-1]
+    kurz = float(kurz.replace(",","."))
+    return kurz
+
+def wrong():
+    print(Fore.RED + "Špatná hodnota!" + Fore.WHITE)
+
+
 for line in lines:
-    if "|EUR|" in line:
-        row = line
-
-print(row)
-
-row_arr = row.split("|")
-
-kurz_str = row_arr[-1]
-kurz_str = float(kurz_str.replace(",","."))
+        row_arr = line.split("|")
+        if len(row_arr)>2:
+            print(Fore.BLUE + row_arr[1] +" - "+ Fore.YELLOW +row_arr[-2])
+print(Fore.BLUE+ "Česká koruna - " + Fore.YELLOW + "CZK" + Fore.WHITE)
 
 
-print(kurz_str)
+while (1):
+
+    example="(" +Fore.YELLOW+ "EUR" +Fore.WHITE+ "/" +Fore.YELLOW+ "CZK" +Fore.WHITE+ "/" +Fore.YELLOW+"USD" +Fore.WHITE+ ")"
+
+    curr1 = (input(f"Napište zkratku původní měny {example}")).upper()
+    curr2 = (input(f"Napište zkratku výsledné měny {example}")).upper()
+    kurz1 = get_kurz(curr1)
+    kurz2 = get_kurz(curr2)
+    if kurz1==-1 or kurz2==-1:
+        wrong()
+    else: break
+
+while (1):
+    value = input(f"Napiště hodnotu ({Fore.YELLOW}{curr1}{Fore.WHITE}) pro převedení:")
+    
+    value = value.replace(",",".")
+
+    try:
+        value = float(value)
+        break
+    except:
+        wrong()
+        value=0
+
+exch = value*kurz1/kurz2
+print(f"Vysledná hodnota: {Fore.GREEN}{value} {Fore.YELLOW}{curr1}{Fore.WHITE} = {Fore.GREEN}{exch} {Fore.YELLOW}{curr2}{Fore.WHITE}")
+
+
+
+
