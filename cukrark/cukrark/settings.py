d5ece1f25692c3ecd06c18fe3e599014bdce1d24
GeneralFlex <vincent.klouda.s@gyarab.cz>
2025-03-11 23:32:11 +0100

    major site update

--- cukrark/cukrark/settings.py
+++ cukrark/cukrark/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "main/static",  # For global static files
+    # Add paths to app static folders if needed, e.g.,
+    # BASE_DIR / "your_app" / "static",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
