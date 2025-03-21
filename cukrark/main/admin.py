94082c4d73d676e223896529571cc3c64a82bd82
Vincent Klouda <vincent.klouda.s@gyarab.cz>
2025-03-21 12:22:35 +0100

    database

--- cukrark/main/admin.py
+++ cukrark/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product
+from .models import Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "price", "category"]
+
+admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+admin.site.register(Category, CategoryAdmin)
--- cukrark/main/models.py
+++ cukrark/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    price = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, default="")
+
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return f"{self.name}"
+
+
