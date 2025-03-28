0696c4ca8ad8f0ccabc1d87e166936f0fb5ea489
Vincent Klouda <vincent.klouda.s@gyarab.cz>
2025-03-28 12:16:03 +0100

    modely

--- cukrark/main/admin.py
+++ cukrark/main/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Product
 from .models import Category
+from .models import User
+from .models import Order
+from .models import Review
+from .models import ImportantPPL
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "price", "category"]
@@ -10,5 +14,20 @@ admin.site.register(Product, ProductAdmin)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["id", "name"]
 
-
 admin.site.register(Category, CategoryAdmin)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email"]
+
+admin.site.register(User, UserAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "price"]
+
+admin.site.register(Order, OrderAdmin)
+
+class ImportantPPLAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "position", "email"]
+
+admin.site.register(ImportantPPL, ImportantPPLAdmin)
+
--- cukrark/main/models.py
+++ cukrark/main/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default="")
     price = models.IntegerField(null=True, blank=True)
     category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
@@ -10,12 +11,47 @@ class Product(models.Model):
         return f"{self.name}"
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default="")
 
     class Meta:
         verbose_name_plural = "Categories"
     
+    def __str__(self):
+        return f"{self.name}"
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    tel = models.CharField(max_length=25, null=True)
+    address = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Review(models.Model):
+    user = models.ForeignKey('User', null=True, on_delete=models.SET_NULL)
+    content = models.TextField(blank=True, default="")
+    stars = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Order(models.Model):
+    user = models.ForeignKey('User', null=True, on_delete=models.CASCADE)
+    #products = models.TextField(blank=True, default="")  ??? []
+    price = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class ImportantPPL(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    tel = models.CharField(max_length=25, null=True)
+    email = models.TextField(blank=True, default="")
+
     def __str__(self):
         return f"{self.name}"
 
