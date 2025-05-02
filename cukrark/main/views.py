079eef5a17c5fceecc12604f7857bff184e61fc6
Vincent Klouda <vincent.klouda.s@gyarab.cz>
2025-05-02 12:21:44 +0200

    bruh fakt nevim :skull:

--- cukrark/main/views.py
+++ cukrark/main/views.py
@@ -2,9 +2,32 @@ from django.shortcuts import render
 from main.models import Product
 
 def get_homepage(request):
+    products = Product.objects.all().order_by('name')
+
+    # filter by name if a query string is present
+    search = request.GET.get('search')
+    if search:
+        products = products.filter(name__icontains=search)
+
     context = {
-        "products": Product.objects.all()
+        "products": products
     }
+
     for product in context['products']:
-        product.img_url = './main/pictures/'+product.image_name+'.png'
+        product.img_url = './main/pictures/'+product.name+'.png'
     return render(request, "main/homepage.html", context)
+
+def get_base(request):
+    context = {
+    }
+    return render(request, "main/base.html", context)
+
+def get_one(request):
+    context = {
+    }
+    return render(request, "main/one.html", context)
+
+def get_two(request):
+    context = {
+    }
+    return render(request, "main/two.html", context)
